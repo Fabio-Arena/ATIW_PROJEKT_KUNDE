178031fb75559faddfbca29ef672d223b35df426
User <farena2001@gmail.com>
2021-05-04 08:58:08 +0200

    DATENBANKK + UI

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module org.example {
     requires javafx.controls;
     requires javafx.fxml;
+    requires ojdbc8;
+    requires java.sql;
+    requires java.naming;
 
     opens org.example to javafx.fxml;
     exports org.example;
--- src/main/java/org/example/ODB.java
+++ src/main/java/org/example/ODB.java
@@ -1,4 +1,108 @@
 package org.example;
 
+import oracle.jdbc.pool.OracleDataSource;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
 public class ODB {
+    private String user;
+    private String passwd;
+    private String host;
+    private String port;
+    private String service;
+    private String connectionString;
+    Connection con;
+
+    public ODB(String user, String passwd, String host, String port, String service) {
+        this.user = user;
+        this.passwd = passwd;
+        this.host = host;
+        this.port = port;
+        this.service = service;
+        this.connectionString ="jdbc:oracle:thin:@(DESCRIPTION="
+                + "(ADDRESS=(PROTOCOL=tcp)(HOST=" + host + ")(PORT=" + port + "))"
+                + "(CONNECT_DATA=(SERVICE_NAME=" + service + ")))";
+        con=null;
+    }
+
+    public void connect(){
+        try{
+            OracleDataSource dataSource = new OracleDataSource();
+
+            dataSource.setURL(connectionString);
+            dataSource.setUser(user);
+            dataSource.setPassword(passwd);
+
+            con = dataSource.getConnection();
+
+            System.out.println("Verbindung steht!");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public String getList(String sql){
+        String ausgabe="";
+        try {
+            Statement befehl = con.createStatement();
+
+            ResultSet ergebnisse = befehl.executeQuery(sql);
+
+            while(ergebnisse.next()) {
+                String name = ergebnisse.getString("name");
+                String vorname = ergebnisse.getString("vorname");
+                int pnr = ergebnisse.getInt("personal_nr");
+                Date geb = ergebnisse.getDate("Geburtstag");
+
+                ausgabe += "" + pnr + ", " + name + ", " + vorname + ", " + geb + "\n";
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return ausgabe;
+    }
+
+    public void createTable(String name){
+        Statement befehl = null;
+        try {
+            befehl = con.createStatement();
+
+            //befehl.execute("DROP TABLE "+ name);
+            befehl.execute("CREATE TABLE "+ name + " ( name varchar2(50), vorname varchar2(50))");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void insertData(String name, String vorname, Date geburtstag, String geschlecht, String stadt,String strasse){
+        Statement befehl = null;
+        try {
+            befehl = con.createStatement();
+
+            befehl.execute("INSERT INTO Test VALUES ('"+ name + "', '" + vorname +"')");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void deleteData(int kundenid){
+
+    }
+
+    public void updateData(String name, String vorname, Date geburtstag, String geschlecht, String stadt,String strasse){
+
+    }
+
+    public void close(){
+        try {
+            con.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
