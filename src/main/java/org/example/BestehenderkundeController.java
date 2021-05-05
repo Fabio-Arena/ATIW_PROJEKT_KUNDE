c45436c18a5fea5ccd1b53ac796310c18de038b7
mike kronewald <mike.kronewald@atos.net>
2021-05-05 15:12:16 +0200

    Tabelle

--- src/main/java/org/example/BestehenderkundeController.java
+++ src/main/java/org/example/BestehenderkundeController.java
@@ -16,6 +16,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class BestehenderkundeController implements Initializable {
@@ -26,39 +28,40 @@ public class BestehenderkundeController implements Initializable {
     private TableView kundendaten = new TableView();
 
     @FXML
-    private TableColumn<String, String> spalte_name = new TableColumn<>("Name");
+    private TableColumn spalte_name = new TableColumn("Name");
 
 
     @FXML
-    private TableColumn<String, String> spalte_vorname = new TableColumn<>("Vorname");
+    private TableColumn spalte_vorname = new TableColumn("Vorname");
 
     @FXML
-    private TableColumn<String, String> spalte_geburtstag = new TableColumn<>("Geburtstag");
+    private TableColumn spalte_geburtstag = new TableColumn("Geburtstag");
 
     @FXML
-    private TableColumn<String, String> spalte_geschlecht = new TableColumn<>("Geschlecht");
+    private TableColumn spalte_geschlecht = new TableColumn("Geschlecht");
 
     @FXML
-    private TableColumn<String, String> spalte_stadt = new TableColumn<>("Stadt");
+    private TableColumn spalte_stadt = new TableColumn<>("Stadt");
 
     @FXML
-    private TableColumn<String, String> spalte_strasse = new TableColumn<>("Strasse");
+    private TableColumn spalte_strasse = new TableColumn("Strasse");
 
     private ODB db = new ODB("SUS_FS191_MASTER","m","oracle.s-atiw.de","1521","atiwora");
 
-    public ObservableList<String> data;
+    private  ObservableList<Kunde> data;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         kundendaten.getColumns().clear();
 
-        spalte_name.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
-        spalte_vorname.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
-        spalte_geburtstag.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
-        spalte_geschlecht.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
-        spalte_stadt.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
-        spalte_strasse.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
+        spalte_name.setCellValueFactory(new PropertyValueFactory<Kunde, String>("name"));
+        spalte_vorname.setCellValueFactory(new PropertyValueFactory<Kunde, String>("vorname"));
+        spalte_geburtstag.setCellValueFactory(new PropertyValueFactory<Kunde, String>("geburtstag"));
+        spalte_geschlecht.setCellValueFactory(new PropertyValueFactory<Kunde, String>("geschlecht"));
+        spalte_stadt.setCellValueFactory(new PropertyValueFactory<Kunde, String>("stadt"));
+        spalte_strasse.setCellValueFactory(new PropertyValueFactory<Kunde, String>("strasse"));
         kundendaten.getColumns().addAll(spalte_name, spalte_vorname, spalte_geburtstag, spalte_geschlecht, spalte_stadt, spalte_strasse);
 
 
@@ -70,36 +73,22 @@ public class BestehenderkundeController implements Initializable {
             Statement statement = connection.createStatement();
 
             ResultSet result = statement.executeQuery(sql);
-            ArrayList<String> name=new ArrayList<>();
-            ArrayList<String> vorname=new ArrayList<>();
-            ArrayList<String> geburts=new ArrayList<>();
-            ArrayList<String> gesch=new ArrayList<>();
-            ArrayList<String> stadt =new ArrayList<>();
-            ArrayList<String> strasse=new ArrayList<>();
+
+
+
 
             while (result.next()) {
 
-                name.add(result.getString("name"));
-                vorname.add(result.getString("vorname"));
-                geburts.add(result.getString("geburtstag"));
-                gesch.add(result.getString("geschlecht"));
-                stadt.add(result.getString("stadt"));
-                strasse.add(result.getString("strasse"));
-                data = FXCollections.observableArrayList(result.getString("name"),result.getString("vorname"),result.getString("geburtstag"),result.getString("geschlecht"),result.getString("stadt"),result.getString("strasse"));
+
+                data = FXCollections.observableArrayList(new Kunde(result.getString("name"),result.getString("vorname"),result.getString("geburtstag"),result.getString("geschlecht"),result.getString("stadt"),result.getString("strasse")));
 
 
-                kundendaten.getItems().add(data.get(0));
-                kundendaten.getItems().add(data.get(1));
-                kundendaten.getItems().add(data.get(2));
-                kundendaten.getItems().add(data.get(3));
-                kundendaten.getItems().add(data.get(4));
-                kundendaten.getItems().add(data.get(5));
-
-            }
-            for(int i=0;i<name.size();i++){
-                //data = FXCollections.observableArrayList(name.get(i),vorname.get(i),geburts.get(i),gesch.get(i),stadt.get(i),strasse.get(i));
+                kundendaten.getItems().add(data);
                 //kundendaten.setItems(data);
+
+
             }
+
 
             statement.close();
         } catch (SQLException e) {
---
+++ src/main/java/org/example/Kunde.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Kunde {
+    private String name;
+    private String vorname;
+    private String geb;
+    private String gesch;
+    private String stadt;
+    private String strasse;
+
+    public Kunde(String name, String vorname,String geb, String gesch,String stadt,String strasse){
+        this.name=name;
+        this.vorname=vorname;
+        this.geb=geb;
+        this.gesch=gesch;
+        this.stadt=stadt;
+        this.strasse=strasse;
+    }
+}
