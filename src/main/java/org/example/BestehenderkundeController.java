282139e605478ae2f6cefcdac4a119b112cb27b8
mike kronewald <mike.kronewald@atos.net>
2021-05-05 14:23:20 +0200

    Tabelle

--- src/main/java/org/example/BestehenderkundeController.java
+++ src/main/java/org/example/BestehenderkundeController.java
@@ -15,6 +15,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class BestehenderkundeController implements Initializable {
@@ -69,20 +70,33 @@ public class BestehenderkundeController implements Initializable {
             Statement statement = connection.createStatement();
 
             ResultSet result = statement.executeQuery(sql);
+            ArrayList<String> name=new ArrayList<>();
+            ArrayList<String> vorname=new ArrayList<>();
+            ArrayList<String> geburts=new ArrayList<>();
+            ArrayList<String> gesch=new ArrayList<>();
+            ArrayList<String> stadt =new ArrayList<>();
+            ArrayList<String> strasse=new ArrayList<>();
 
             while (result.next()) {
-                String name = result.getString("name");
-                data = FXCollections.observableArrayList(result.getString("name"),result.getString("vorname"),result.getString("geburtstag"),result.getString("geschlecht"),result.getString("stadt"),result.getString("strasse"));
-                String geburtstag = result.getString("geburtstag");
-                String geschlecht = result.getString("geschlecht");
-                String stadt = result.getString("stadt");
-                String strasse = result.getString("strasse");
+
+                name.add(result.getString("name"));
+                vorname.add(result.getString("vorname"));
+                geburts.add(result.getString("geburtstag"));
+                gesch.add(result.getString("geschlecht"));
+                stadt.add(result.getString("stadt"));
+                strasse.add(result.getString("strasse"));
+                //data = FXCollections.observableArrayList(result.getString("name"),result.getString("vorname"),result.getString("geburtstag"),result.getString("geschlecht"),result.getString("stadt"),result.getString("strasse"));
 
 
-                kundendaten.setItems(data);
+               // kundendaten.setItems(data);
 
 
             }
+            for(int i=0;i<name.size();i++){
+                data = FXCollections.observableArrayList(name.get(i),vorname.get(i),geburts.get(i),gesch.get(i),stadt.get(i),strasse.get(i));
+                kundendaten.setItems(data);
+            }
+
             statement.close();
         } catch (SQLException e) {
             System.out.println("Datenbank fehler");
